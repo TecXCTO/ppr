2d333055216e633b8aa8bc74d3e6b1f9bfd3c694
TecXCTO <cto.tecx@gmail.com>
2026-02-20 18:09:11 +0530

    Update primitive_harmony_resonanse.py

--- primitive_harmony_resonanse.py
+++ primitive_harmony_resonanse.py
@@ -1,10 +1,10 @@
 class PrimitiveParameters(self, *args, **kwargs):
-  max_frequency
-  max_volt
-  max_current
-  max_practical_frequency
-  max_practical_volt
-  max_practical_current
+  max_frequency : ""
+  max_volt : ""
+  max_current : ""
+  max_practical_frequency : ""
+  max_practical_volt : ""
+  max_practical_current : ""
   
   min_frequency
   min_volt
