3572be8abf5aa1450a92f137e9ed52187c595f40
TecXCTO <cto.tecx@gmail.com>
2026-02-21 12:40:56 +0530

    Update frequency_transformer.py

--- frequency_transformer.py
+++ frequency_transformer.py
@@ -1,1 +1,9 @@
-
+import math
+from primitive_harmonic_resonance import PrimitiveParameters
+class FrequencyTransformer(PrimitiveParameters):
+  def __init__( self, *args, **kwargs, parameters):
+    super.__init__()
+    self.inpur_frequency=frequency
+    self.output_frequency=
+  def transformer(self, input_parameter):
+    self.
