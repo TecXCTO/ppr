40ccb51a0949952ff3d023f88e5707662361f2a4
TecXCTO <cto.tecx@gmail.com>
2026-02-21 08:34:40 +0530

    Update primitive_harmony_resonanse.py

--- primitive_harmony_resonanse.py
+++ primitive_harmony_resonanse.py
@@ -15,6 +15,64 @@ class PrimitiveParameters():
       min_practical_frequency   : "",
       min_practical_volt  : "" ,
       min_practical_current : "",
+      max_electric_field : "",
+      min_electric_field : "",
+      max_electric_field_frequency : "",
+      min_electric_field_frequency : "",
+      max_electric_field__wavelength : "",
+      min_electric_field_wavelength : "",
+      max_electric_field_speed : "1/math.sqrt(vaccum_permittivity*vaccum_permeability)", # Permittivity is critical in determining the speed of electromagnetic waves
+      min_electric_field_speed : "1/math.sqrt(material_permittivity*material_permeability)",
+      max_magnetic_field : "",
+      min_magnetic_field : "",
+      max_magnetic_field_frequency : "",
+      min_magnetic_field_frequency : "",
+      max_magnetic_field__wavelength : "",
+      min_magnetic_field__wavelength : "",
+      max_magnetic_field_speed : "1/math.sqrt(vaccum_permittivity*vaccum_permeability)",
+      min_magnetic_field_speed : "1/math.sqrt(material_permittivity*material_permeability)",
+      vaccum_permittivity : "8.854*10e-12", # unit in F/M Farad per meter of SI
+      reletive_permittivity : "",
+      material_permittivity : "",
+      vaccum_permeability : "4*phi*10e-7",
+      reletive_permeability: "",
+      material_permeability: "",
+      light_speed: "299792458", # In Vaccum Meter per Second.
+      phi: "3.141592653589793"
+    }
+    self.electricity_corresponding_parameters = {
+      max_frequency : "",
+      max_volt_corresponding_to_max_frequency: "",
+      max_current_corresponding_to_max_frequency : "",
+      min_volt_corresponding_to_max_frequency: "",
+      min_current_corresponding_to_max_frequency : "",
+      max_practical_frequency : "",
+      max_volt_corresponding_to_max_practical_frequency: "",
+      max_current_corresponding_to_max_practical_frequency : "",
+      max_practical_volt_corresponding_to_max_practical_frequency: "",
+      max_practical_current_corresponding_to_max_practical_frequency : "",
+      min_practical_volt_corresponding_to_max_practical_frequency: "",
+      min_practical_current_corresponding_to_max_practical_frequency : "",
+      min_volt_corresponding_to_max_practical_frequency: "",
+      min_current_corresponding_to_max_practical_frequency : "",
+      
+      max_practical_volt : "",
+      
+      max_practical_current : "",
+      
+      min_frequency  : "",
+      
+      
+      min_volt   : "",
+      
+      min_current   : "",
+      
+      min_practical_frequency   : "",
+      
+      min_practical_volt  : "" ,
+      
+      min_practical_current : "",
+      
       light_speed: "299792458", # In Vaccum Meter per Second.
       phi: "3.141592653589793"
     }
